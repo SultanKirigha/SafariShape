6a7f2ea814cdf4fd763062047d9e80fecf1c2435
Sultan Kirigha <sultankirigha@gmail.com>
2025-01-27 15:17:09 +0300

    implement the footer of the site

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,8 +1,22 @@
+import Link from 'next/link'
 import React from 'react'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="border-t">
+      <div className='flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row'>
+        <Link href='/'>
+          <Image
+            src="/assets/images/safarilogo.svg"
+            alt="logo"
+            width={128}
+            height={38}
+          />
+        </Link>
+        <p>2025 Safari Shape. All Rights reserved.</p>
+      </div>
+    </footer>
   )
 }
 
